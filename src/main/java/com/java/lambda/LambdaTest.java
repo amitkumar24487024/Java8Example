e066a35145202cee919a17e453cc0d8cc951726e
amitkumar24487024 <amitkumar2448@gmail.com>
2020-03-12 00:51:17 +0530

    added Lambda Function

--- src/main/java/com/java/lambda/LambdaTest.java
+++ src/main/java/com/java/lambda/LambdaTest.java
@@ -1,10 +1,36 @@
 package com.java.lambda;
 
+interface Addition{
+	int add(int x,int y);
+}
+
+class AdditionTest implements Addition{
+
+	public int add(int x, int y) {
+		// TODO Auto-generated method stub
+		return x+y;
+	}
+	
+}
+
+
+
 public class LambdaTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Lambda Test new");
+		System.out.println("Lambda Test new");
+		
+		AdditionTest add=new AdditionTest();
+		int x=add.add(10,20);
+		System.out.println("Sum is "+x);
+		
+		
+		// Lambda Way
+		
+		Addition add1=(a,b)->a+b;
+		System.out.println("Lambda Addition : "+add1.add(50,75));
+		
+	//	System.out.println("Lambda Addition : "+(a,b)->a+b);
 	}
 
 }
